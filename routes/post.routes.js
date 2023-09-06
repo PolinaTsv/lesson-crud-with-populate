4437396d7413fa7af2d360f00c6e36fc8f82bab1
PolinaTsv <polina.tsvetkova7@gmail.com>
2023-09-06 15:53:21 +0700

    No styles

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -13,7 +13,9 @@ router.get("/post-create", (req, res) => {
     .then((dbUsers) => {
       res.render("posts/create", { dbUsers });
     })
-    .catch((err) => console.log(`Err while displaying post input page: ${err}`));
+    .catch((err) =>
+      console.log(`Err while displaying post input page: ${err}`)
+    );
 });
 
 // ****************************************************************************************
@@ -22,19 +24,61 @@ router.get("/post-create", (req, res) => {
 
 // <form action="/post-create" method="POST">
 
-// ... your code here
+router.post("/post-create", (req, res, next) => {
+  const { title, content, author } = req.body;
+  // 'author' represents the ID of the user document
+
+  Post.create({ title, content, author })
+    .then((dbPost) => {
+      // when the new post is created, the user needs to be found and its posts updated with the
+      // ID of newly created post
+      return User.findByIdAndUpdate(author, { $push: { posts: dbPost._id } });
+    })
+    .then(() => res.redirect("/posts")) // if everything is fine, redirect to list of posts
+    .catch((err) => {
+      console.log(`Err while creating the post in the DB: ${err}`);
+      next(err);
+    });
+});
 
 // ****************************************************************************************
 // GET route to display all the posts
 // ****************************************************************************************
 
-// ... your code here
+router.get("/posts", (req, res, next) => {
+  Post.find()
+    .then((dbPosts) => {
+      console.log("Posts from the DB: ", dbPosts);
+    })
+    .catch((err) => {
+      console.log(`Err while getting the posts from the DB: ${err}`);
+      next(err);
+    });
+});
 
 // ****************************************************************************************
 // GET route for displaying the post details page
 // shows how to deep populate (populate the populated field)
 // ****************************************************************************************
 
-// ... your code here
+router.get("/posts/:postId", (req, res, next) => {
+  const { postId } = req.params;
+
+  Post.findById(postId)
+    .populate("author comments") // <-- the same as .populate('author).populate('comments')
+    .populate({
+      // we are populating author in the previously populated comments
+      path: "comments",
+      populate: {
+        path: "author",
+        model: "User",
+      },
+    })
+    .then((foundPost) => res.render("posts/details", foundPost))
+    .catch((err) => {
+      console.log(`Err while getting a single post from the  DB: ${err}`);
+      next(err);
+    });
+});
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -21,7 +21,9 @@ router.post("/user-create", (req, res) => {
         User.create({ username })
         .then(() => res.redirect('/post-create'));
       } else {
-        res.render("users/create", { message: "It seems you are already registered. ☀️" });
+        res.render("users/create", {
+          message: "It seems you are already registered. ☀️",
+        });
         return;
       }
     })
@@ -35,13 +37,23 @@ router.post("/user-create", (req, res) => {
 router.get("/users", (req, res) => {
   User.find() // <-- .find() method gives us always an ARRAY back
     .then((usersFromDB) => res.render("users/list", { users: usersFromDB }))
-    .catch((err) => console.log(`Error while getting users from the DB: ${err}`));
+    .catch((err) =>
+      console.log(`Error while getting users from the DB: ${err}`)
+    );
 });
 
 // ****************************************************************************************
 // GET details of a specific user (primarily their posts)
 // ****************************************************************************************
 
-// ... your code here
+router.get("/users/:userId/posts", (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId) // <-- .findById() method gives us always an OBJECT back
+    .populate("posts")
+    .then((user) => res.render("users/details", user))
+    .catch((err) =>
+      console.log(`Error while getting user details from the DB: ${err}`)
+    );
+});
 
 module.exports = router;
